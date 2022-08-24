8933c417dee2e15be11d7bf3dbbe7288c5c4ce63
amolmane7471 <maneamol3233@gmail.com>
2022-08-24 16:14:35 +0530

    [Amol]:added emp wages Uc5 for calculating total wages for a month

--- src/com/bridgelabz/empwage/EmpWage.java
+++ src/com/bridgelabz/empwage/EmpWage.java
@@ -4,10 +4,13 @@ public class EmpWage {
 	final int isFullTime = 1;
 	final int isPartTime = 2;
 	final int empWagePerHrs = 20;
+	final int numOfWorkingDays = 20;
 	int empHrs ;
 	int empWage;
+	int totalEmpWage = 0;
 	
 	public void computeEmpWage() {
+	for(int day = 1; day <= numOfWorkingDays; day++) {
 	int empcheck = (int) Math.floor(Math.random() * 10 % 3);
 	switch(empcheck) {
 	case isFullTime:
@@ -24,8 +27,10 @@ public class EmpWage {
 		break;
 	}
 	empWage = empHrs * empWagePerHrs;
-	System.out.println("daily Emp Wage Is : "+empWage);
-
+	totalEmpWage += empWage;
+	System.out.println("daily Emp Wage for "+day+ " Is : "+empWage);
+	}
+	System.out.println("\n total Emp Wage is: "+totalEmpWage);
 	}
 	public static void main(String args[]){
 		System.out.println("<<< Welcome To Employee Wage computation program >>>");
