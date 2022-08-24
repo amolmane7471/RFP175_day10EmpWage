ed3a21ba3b7451bf87f116a720104bde7d44d300
amolmane7471 <maneamol3233@gmail.com>
2022-08-24 18:03:45 +0530

    [Amol]:added emp wages Uc6 for calculating total wages for a month or working hrs in a month is reached

--- src/com/bridgelabz/empwage/EmpWage.java
+++ src/com/bridgelabz/empwage/EmpWage.java
@@ -5,12 +5,15 @@ public class EmpWage {
 	final int isPartTime = 2;
 	final int empWagePerHrs = 20;
 	final int numOfWorkingDays = 20;
-	int empHrs ;
-	int empWage;
+	final int maxHrsInMonth = 100;
+	int empHrs;
+	int totalEmpHrs = 0 ;
+	int totalWorkingDays = 0 ;
 	int totalEmpWage = 0;
 	
 	public void computeEmpWage() {
-	for(int day = 1; day <= numOfWorkingDays; day++) {
+	while(totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays ) {
+		totalWorkingDays ++;
 	int empcheck = (int) Math.floor(Math.random() * 10 % 3);
 	switch(empcheck) {
 	case isFullTime:
@@ -26,12 +29,13 @@ public class EmpWage {
 		empHrs = 0;
 		break;
 	}
-	empWage = empHrs * empWagePerHrs;
-	totalEmpWage += empWage;
-	System.out.println("daily Emp Wage for "+day+ " Is : "+empWage);
+	totalEmpHrs += empHrs;
+	System.out.println("day:"+totalWorkingDays+ " Emp Hr : " +totalEmpHrs);
 	}
+	totalEmpWage = totalEmpHrs * empWagePerHrs;
 	System.out.println("\n total Emp Wage is: "+totalEmpWage);
 	}
+	
 	public static void main(String args[]){
 		System.out.println("<<< Welcome To Employee Wage computation program >>>");
 		EmpWage attendance = new EmpWage();
